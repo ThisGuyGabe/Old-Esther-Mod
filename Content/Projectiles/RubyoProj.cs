b6f18a9b8e601aee875d04d914549fb30445299d
ThisGuyGabe <127223143+ThisGuyGabe@users.noreply.github.com>
2023-04-27 23:35:09 -0400

    Update RubyoProj.cs

--- Content/Projectiles/RubyoProj.cs
+++ Content/Projectiles/RubyoProj.cs
@@ -4,11 +4,19 @@ using Terraria.ModLoader;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Terraria.GameContent;
+using System;
 
 namespace EstherMod.Content.Projectiles
 {
     public class RubyoProj : ModProjectile
     {
+        const int ShootRate = 60;
+        const float ShootDistance = 500f;
+        const float ShootSpeed = 10f;
+        const int ShootDamage = 38;
+        const float ShootKnockback = 4f;
+        int ShootType = -1;
+        int TimeToShoot = ShootRate;
         public override void SetStaticDefaults()
         {
             // DisplayName.SetDefault("Rubyo Proj");
@@ -29,9 +37,38 @@ namespace EstherMod.Content.Projectiles
             Projectile.friendly = true;
         }
 
+        public static Vector2 VelocityToPoint(Vector2 A, Vector2 B, float Speed)
+        {
+            var Move = B - A;
+            return Move * (Speed / (float)Math.Sqrt(Move.X * Move.X + Move.Y * Move.Y));
+        }
         public override void AI()
         {
-            
+            if (--TimeToShoot <= 0)
+            {
+                TimeToShoot = ShootRate;
+                if (ShootType == -1)
+                    ShootType = Mod.Find<ModProjectile>("Ruby").Type;
+
+                float NearestNPCDist = ShootDistance;
+                int NearestNPC = -1;
+                foreach (NPC npc in Main.npc)
+                {
+                    if (!npc.active)
+                        continue;
+                    if (npc.friendly || npc.lifeMax <= 5)
+                        continue;
+                    if (NearestNPCDist == -1 || npc.Distance(Projectile.Center) < NearestNPCDist && Collision.CanHitLine(Projectile.Center, 16, 16, npc.Center, 16, 16))
+                    {
+                        NearestNPCDist = npc.Distance(Projectile.Center);
+                        NearestNPC = npc.whoAmI;
+                    }
+                }
+                if (NearestNPC == -1)
+                    return;
+                Vector2 Velocity = VelocityToPoint(Projectile.Center, Main.npc[NearestNPC].Center, ShootSpeed);
+                Projectile.NewProjectile(Projectile.GetSource_NaturalSpawn(), Projectile.Center.X, Projectile.Center.Y, Velocity.X, Velocity.Y, ShootType, ShootDamage, ShootKnockback, Projectile.owner);
+            }
         }
         public override bool PreDraw(ref Color lightColor)
         {
