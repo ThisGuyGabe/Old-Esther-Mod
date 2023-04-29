b3a972b68189f4111401c70dd7acf982a0d6628b
ThisGuyGabe <127223143+ThisGuyGabe@users.noreply.github.com>
2023-04-29 19:37:30 -0400

    Add files via upload

---
+++ Content/Items/Placeable/UmbraStone.cs
@@ -0,0 +1,46 @@
+using Terraria.GameContent.Creative;
+using Terraria.ModLoader;
+using static Terraria.ModLoader.ModContent;
+using EstherMod.Content.Tiles;
+using Terraria.ID;
+using Terraria.DataStructures;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using Terraria;
+using Terraria.Audio;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.GameContent;
+using Terraria.DataStructures;
+using Terraria.ModLoader.Utilities;
+using Terraria.GameContent.Creative;
+using Terraria.Localization;
+using System.Collections.Generic;
+
+namespace EstherMod.Content.Items.Placeable
+{
+    public class UmbraStone : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            // DisplayName.SetDefault("Umbra Stone");
+            // Tooltip.SetDefault("");
+            CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 100;
+        }
+
+        public override void SetDefaults()
+        {
+            Item.width = 14;
+            Item.height = 14;
+            Item.maxStack = 999;
+            Item.createTile = TileType<UmbraStoneTile>();
+            Item.useTurn = true;
+            Item.autoReuse = true;
+            Item.useAnimation = 15;
+            Item.useTime = 10;
+            Item.useStyle = 1;
+            Item.consumable = true;
+        }
+    }
+}
