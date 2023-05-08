bf8e86c5d43531ca5653eb5ea21557e565b2cde3
ThatGuyGabe <127223143+ThisGuyGabe@users.noreply.github.com>
2023-05-08 02:50:04 -0400

    Armor doesn't do anything atm
    Work In Progress of course but this will be done sometime.

---
+++ Content/Items/Armor/AquaticBoots.cs
@@ -0,0 +1,30 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+using Terraria.ID;
+using Terraria.GameContent.Creative;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+
+namespace EstherMod.Content.Items.Armor
+{
+	[AutoloadEquip(EquipType.Legs)]
+	public class AquaticBoots : ModItem
+	{
+		public override void SetStaticDefaults() 
+		{
+			/* Tooltip.SetDefault("Charged to perfection."
+				+ "\n10% increased movement speed and double jump"); */
+
+			CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;
+		}
+
+		public override void SetDefaults() 
+		{
+			Item.width = 22;
+			Item.height = 18;
+			Item.value = Item.sellPrice(silver: 45);
+			Item.rare = ItemRarityID.LightRed; 
+			Item.defense = 0;
+		}
+	}
+}
---
+++ Content/Items/Armor/AquaticDiverHelmet.cs
@@ -0,0 +1,35 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+using Terraria.ID;
+using Terraria.GameContent.Creative;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+
+namespace EstherMod.Content.Items.Armor
+{
+	[AutoloadEquip(EquipType.Head)]
+	public class AquaticDiverHelmet : ModItem
+	{
+		public override void SetStaticDefaults() 
+		{
+			// Tooltip.SetDefault("");
+
+			CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;
+
+		}
+
+		public override void SetDefaults() 
+		{
+			Item.width = 32;
+			Item.height = 34;
+			Item.value = Item.sellPrice(silver: 75);
+			Item.rare = ItemRarityID.LightRed;
+			Item.defense = 0;
+		}
+
+		public override bool IsArmorSet(Item head, Item body, Item legs) 
+		{
+			return body.type == ModContent.ItemType<AquaticPlating>() && legs.type == ModContent.ItemType<AquaticBoots>();
+		}
+	}
+}
