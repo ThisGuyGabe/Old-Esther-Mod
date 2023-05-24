3dbf34ec9ac9e8d1013a9154f43e387b2571b0e9
Stranza <115468538+Stranza@users.noreply.github.com>
2023-05-24 20:31:10 +0200

    Added Sytems Folder and OreOnDeathSystem.cs

---
+++ Common/Systems/OreOnDeathSystem.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using Microsoft.Xna.Framework;
+using System.Collections.Generic;
+using System.IO;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.ModLoader.IO;
+using Terraria.WorldBuilding;
+using EstherMod.Content.Tiles;
+
+namespace EstherMod.Common.Systems
+{
+	public class OreOnDeathSystem : GlobalNPC
+	{
+		public static bool spawnHypnoplate = false;
+
+		public override void OnKill(NPC npc) {
+			if (npc.type == NPCID.BrainofCthulhu && !NPC.downedBoss2) {
+
+				if (!spawnHypnoplate) { // Spawns Hypno Plate in the world when Brain of Cthulu is defeated.
+                    Main.NewText("Hypnoplating spawns upon the worlds skies.", 255, 0, 0);
+                    for (int k = 0; k < (int)(Main.maxTilesX * Main.maxTilesY * 4E-05); k++)
+            		{
+                		int x = WorldGen.genRand.Next(0, Main.maxTilesX);
+
+                		int y = WorldGen.genRand.Next(0, (int)((int)Main.worldSurface * 0.35D));
+                		int snowCheckY = WorldGen.genRand.Next((int)(Main.worldSurface - 5), (int)(Main.worldSurface + 5));
+
+                		Tile snowCheck = Framing.GetTileSafely(x, snowCheckY);
+                		if (!(snowCheck.TileType == TileID.SnowBlock || snowCheck.TileType == TileID.IceBlock))
+                   			WorldGen.TileRunner(x, y, WorldGen.genRand.Next(6, 8), WorldGen.genRand.Next(6, 8), ModContent.TileType<Hypnoplate>(), true);
+            		}
+
+                	spawnHypnoplate = true;
+				}
+			}
+		}
+	}
+}
