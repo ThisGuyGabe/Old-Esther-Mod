8bb07783eeb3bac05ed1a3b67a5f76bb06536890
Stranza <115468538+Stranza@users.noreply.github.com>
2023-06-06 00:10:03 +0200

    Phonix Flame bow custom projectile

---
+++ Content/Projectiles/PhoenixFlameProjectile.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using Terraria;
+using Terraria.Audio;
+using Terraria.GameContent;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace EstherMod.Content.Projectiles
+{
+	public class PhoenixFlameProjectile : ModProjectile
+	{
+		public override void SetStaticDefaults() {
+		}
+
+		public override void SetDefaults() {
+			Projectile.aiStyle = 1;
+			Projectile.width = 7;
+			Projectile.height = 16; 
+			Projectile.aiStyle = 1; 
+			Projectile.friendly = true;
+			Projectile.DamageType = DamageClass.Ranged; 
+			Projectile.penetrate = -1; 
+			Projectile.timeLeft = 60 * 20; 
+			Projectile.light = 0.25f;
+			Projectile.ignoreWater = true; 
+			Projectile.tileCollide = true; 
+
+			AIType = ProjectileID.WoodenArrowFriendly; 
+		}
+
+		public override void AI() {
+
+			int dustnumber = Dust.NewDust(Projectile.position, Projectile.width, Projectile.height, DustID.Torch, 0f, 0f, 0, default(Color), 0.8f);
+			Main.dust[dustnumber].velocity *= 1.3f;
+			//Main.dust[dust].noGravity = true;
+
+		}
+
+		public override void OnHitNPC(NPC target, NPC.HitInfo hit, int damageDone) {
+			target.AddBuff(BuffID.Daybreak, 60 * 2);
+		}
+
+		public override bool PreDraw(ref Color lightColor) {
+			Main.instance.LoadProjectile(Projectile.type);
+			Texture2D texture = TextureAssets.Projectile[Projectile.type].Value;
+
+			Vector2 drawOrigin = new Vector2(texture.Width * 0.5f, Projectile.height * 0.5f);
+			for (int k = 0; k < Projectile.oldPos.Length; k++) {
+				Vector2 drawPos = (Projectile.oldPos[k] - Main.screenPosition) + drawOrigin + new Vector2(0f, Projectile.gfxOffY);
+				Color color = Projectile.GetAlpha(lightColor) * ((Projectile.oldPos.Length - k) / (float)Projectile.oldPos.Length);
+				Main.EntitySpriteDraw(texture, drawPos, null, color, Projectile.rotation, drawOrigin, Projectile.scale, SpriteEffects.None, 0);
+			}
+
+			return true;
+		}
+
+		public override void Kill(int timeLeft) {
+			Collision.HitTiles(Projectile.position + Projectile.velocity, Projectile.velocity, Projectile.width, Projectile.height);
+			SoundEngine.PlaySound(SoundID.Item10, Projectile.position);
+		}
+
+	}
+}
