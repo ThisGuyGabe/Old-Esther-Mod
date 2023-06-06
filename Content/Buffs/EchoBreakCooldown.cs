eaef4da93d7dd0ec52afcc0154bf2a28bd64a40a
ThatGuyGabe <127223143+ThisGuyGabe@users.noreply.github.com>
2023-06-06 17:35:59 -0400

    Add files via upload

---
+++ Content/Buffs/EchoBreakCooldown.cs
@@ -0,0 +1,18 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace EstherMod.Content.Buffs
+{
+    public class EchoBreakCooldown : ModBuff
+    {
+        public override void SetStaticDefaults()
+        {
+            // DisplayName.SetDefault("Echo Break Cooldown");
+            // Description.SetDefault("You can't use Echo Break now!");
+            Main.buffNoTimeDisplay[Type] = false;
+            Main.debuff[Type] = true; //Add this so the nurse doesn't remove the buff when healing
+            BuffID.Sets.NurseCannotRemoveDebuff[Type] = true;
+        }
+    }
+}
