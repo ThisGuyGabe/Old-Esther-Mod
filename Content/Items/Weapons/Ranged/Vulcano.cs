8a0eac1ccd07eb27253b040b40b814016379e879
Stranza <115468538+Stranza@users.noreply.github.com>
2023-06-08 22:15:07 +0200

    Reworked Vulcano

--- Content/Items/Weapons/Ranged/Vulcano.cs
+++ Content/Items/Weapons/Ranged/Vulcano.cs
@@ -20,42 +20,78 @@ namespace EstherMod.Content.Items.Weapons.Ranged
         public override void SetDefaults()
         {
             Item.damage = 71;
-            Item.crit = 21;
+            Item.crit = 10;
             Item.rare = ItemRarityID.LightRed;
             Item.width = 32;
             Item.height = 72;
-            Item.useAnimation = 16;
-            Item.useTime = 8;
+            Item.useAnimation = 27;
+            Item.useTime = 27;
             Item.useStyle = 5;
             Item.knockBack = 4.5f;
-            Item.shootSpeed = 17f;
+            Item.shootSpeed = 15f;
             Item.DamageType = DamageClass.Ranged;
             Item.autoReuse = true;
             Item.noMelee = true;
-            Item.value = Item.sellPrice(gold: 5, silver: 67);
+            Item.shoot = 1;
+            Item.value = Item.sellPrice(gold: 25, silver: 67);
             Item.UseSound = SoundID.Item5;
-            Item.shoot = 28;
             Item.useAmmo = AmmoID.Arrow;
-            Item.reuseDelay = 10;
         }
 
-        public override bool CanConsumeAmmo(Item ammo, Player player)
-        {
-            return !(player.itemAnimation < Item.useAnimation - 2);
+        public override void ModifyShootStats(Player player, ref Vector2 position, ref Vector2 velocity, ref int type, ref int damage, ref float knockback) {
+             if (type == ProjectileID.WoodenArrowFriendly) {
+                type = Mod.Find<ModProjectile>("VulcanoProjectile").Type;
+            }
+
+            if (type == ProjectileID.FireArrow) {
+                type = Mod.Find<ModProjectile>("VulcanoProjectile").Type;
+            }
+
+            if (type == ProjectileID.FrostburnArrow) {
+                type = Mod.Find<ModProjectile>("VulcanoProjectile").Type; 
+            }
+
+            if (type == ProjectileID.HellfireArrow) {
+                type = Mod.Find<ModProjectile>("VulcanoProjectile").Type;
+            }
+
         }
 
-        public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
-        {
-            Projectile.NewProjectile(Projectile.GetSource_NaturalSpawn(), position.X, position.Y, velocity.X, velocity.Y, ProjectileID.FireArrow, damage, knockback, player.whoAmI);
-            return true;
-        }
+        public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback) {
+            Vector2 target = Main.screenPosition + new Vector2(Main.mouseX, Main.mouseY);
+            float ceilingLimit = target.Y;
+            if (ceilingLimit > player.Center.Y - 200f) {
+                ceilingLimit = player.Center.Y - 200f;
+            }
+            // Loop these functions 12 times.
+            for (int i = 0; i < 12; i++) {
+                position = player.Center - new Vector2(Main.rand.NextFloat(401) * player.direction, 600f);
+                position.Y -= 200 * i;
+                Vector2 heading = target - position;
 
+                if (heading.Y < 0f) {
+                    heading.Y *= -1f;
+                }
+
+                if (heading.Y < 20f) {
+                    heading.Y = 20f;
+                }
+
+                heading.Normalize();
+                heading *= velocity.Length();
+                heading.Y += Main.rand.Next(-40, 41) * 0.02f;
+                Projectile.NewProjectile(source, position, heading, type, damage, knockback, player.whoAmI, 0f, ceilingLimit);
+            }
+
+            return false;
+        }
 
         public override void AddRecipes()
         {
             Recipe recipe = CreateRecipe();
             recipe.AddIngredient(ItemID.SoulofFright, 10);
             recipe.AddIngredient(Mod.Find<ModItem>("ForgedFury").Type, 1);
+            recipe.AddIngredient(ItemID.DaedalusStormbow, 1);
             recipe.AddIngredient(ItemID.HallowedBar, 20);
             recipe.AddTile(TileID.MythrilAnvil);
             recipe.Register();
