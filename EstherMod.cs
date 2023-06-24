2c4e6cf9d0ea8c440f2243d346812241187069bb
ThatGuyGabe <127223143+ThisGuyGabe@users.noreply.github.com>
2023-06-24 03:20:51 -0400

    There u go Basically I am Little Cat no more dirty code

--- EstherMod.cs
+++ EstherMod.cs
@@ -1,16 +1,13 @@
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.Graphics.Effects;
+using Terraria.Graphics.Shaders;
+using Terraria.ID;
+using Terraria.ModLoader;
+using ReLogic.Content;
+
 namespace EstherMod
 {
-    using System;
-    using Microsoft.Xna.Framework;
-    using Microsoft.Xna.Framework.Graphics;
-    using Terraria;
-    using Terraria.Graphics.Effects;
-    using Terraria.Graphics.Shaders;
-    using Terraria.ID;
-    using Terraria.ModLoader;
-    using static Terraria.ModLoader.ModContent;
-    using ReLogic.Content;
-
     public class EsthrMod : Mod
     {
 
@@ -19,15 +16,6 @@ namespace EstherMod
             if (Main.netMode != NetmodeID.Server)
             {
                 Ref<Effect> screenRef = new Ref<Effect>(ModContent.Request<Effect>("EstherMod/Effects/CustomScreenShaders", AssetRequestMode.ImmediateLoad).Value);
-
-                Filters.Scene["HydraShockwave"] = new Filter(new ScreenShaderData(screenRef, "HydraShockwave"), EffectPriority.VeryHigh);
-                Filters.Scene["HydraShockwave"].Load();
-                Filters.Scene["Confetti"] = new Filter(new ScreenShaderData(screenRef, "Confetti"), EffectPriority.VeryHigh);
-                Filters.Scene["Confetti"].Load();
-
-                Filters.Scene["UnderworldFilter"] = new Filter(new ScreenShaderData(screenRef, "UnderworldFilter"), EffectPriority.VeryHigh);
-                Filters.Scene["UnderworldFilter"].Load();
-
 
                 Filters.Scene["Shockwave"] = new Filter(new ScreenShaderData(screenRef, "Shockwave"), EffectPriority.VeryHigh);
                 Filters.Scene["Shockwave"].Load();
