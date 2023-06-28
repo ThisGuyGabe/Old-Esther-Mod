0ddf357095a590dbce4024cb2a630764ca6ae037
Stranza <115468538+Stranza@users.noreply.github.com>
2023-06-28 23:51:12 +0200

    Sludge Slider Projectile(Fuck off mati)

---
+++ Content/Projectiles/SludgeSliderProjectile.cs
@@ -0,0 +1,41 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace EstherMod.Content.Projectiles
+{
+	public class SludgeSliderProjectile : ModProjectile
+	{
+		public override void SetStaticDefaults() {
+		
+			ProjectileID.Sets.YoyosLifeTimeMultiplier[Projectile.type] = 5.5f;
+
+			ProjectileID.Sets.YoyosMaximumRange[Projectile.type] = 250f;
+
+
+			ProjectileID.Sets.YoyosTopSpeed[Projectile.type] = 13.5f;
+		}
+
+		public override void SetDefaults() {
+			Projectile.width = 14; 
+			Projectile.height = 14;
+			Projectile.scale = 1.3f;
+
+			Projectile.aiStyle = ProjAIStyleID.Yoyo;
+
+			Projectile.friendly = true; 
+			Projectile.DamageType = DamageClass.MeleeNoSpeed; 
+			Projectile.penetrate = -1; 
+		}
+
+		public override void OnHitNPC(NPC target, NPC.HitInfo hit, int damageDone) {
+				target.AddBuff(BuffID.Slimed, 60 * 5);
+		}
+
+		public override void PostAI() {
+ 		int dust = Dust.NewDust(Projectile.position, Projectile.width, Projectile.height, 56, 0f, 0f, 5);// the 56 means slimy saddle dusts.
+ 		Main.dust[dust].noGravity = false; 
+			
+		}
+	}
+}
