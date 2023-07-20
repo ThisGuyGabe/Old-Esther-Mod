037c153f744b8503b63913d4390586dfbe01535a
Karve <124748872+mogusguy442@users.noreply.github.com>
2023-07-20 18:49:53 -0500

    Add files via upload

---
+++ Content/Items/Weapons/Ranged/CactusBow.cs
@@ -0,0 +1,40 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace CascadeMod.Content.Items.Weapons.Ranged
+{
+	public class CactusBow : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+            Item.ResearchUnlockCount = 1;
+        }
+
+		public override void SetDefaults()
+		{
+			Item.damage = 8;
+			Item.DamageType = DamageClass.Ranged;
+			Item.width = 20;
+			Item.height = 38;
+			Item.useTime = 30;
+			Item.useAnimation = 30;
+			Item.useStyle = ItemUseStyleID.Shoot;
+			Item.noMelee = true;
+			Item.knockBack = 2;
+			Item.value = 1800;
+			Item.rare = ItemRarityID.White;
+			Item.UseSound = SoundID.Item5;
+			Item.shoot = ProjectileID.WoodenArrowFriendly;
+			Item.shootSpeed = 5f;
+			Item.useAmmo = AmmoID.Arrow;
+		}
+		public override void AddRecipes()
+        {
+            Recipe recipe = CreateRecipe();
+            recipe.AddIngredient(ItemID.Cactus, 10);
+            recipe.AddTile(TileID.WorkBenches);
+            recipe.Register();
+        }
+	}
+}
