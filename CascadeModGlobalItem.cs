8e53ea3a0606e6cfc43d8bb19ae3783e6d31dcfa
ThatGuyGabe <gabrielnovoa2021@outlook.com>
2023-07-24 00:30:45 -0400

    The Deletion

--- CascadeModGlobalItem.cs
+++ CascadeModGlobalItem.cs
@@ -1,20 +1,10 @@
-using CascadeMod.Content.Items.Weapons.Melee;
 using Terraria;
-using Terraria.GameContent.ItemDropRules;
-using Terraria.ID;
 using Terraria.ModLoader;
 
 namespace CascadeMod {
 	public class CascadeModGlobalItem : GlobalItem {
 		public override void SetDefaults(Item item) {
 			item.autoReuse = true;
-		}
-		public override void ModifyItemLoot(Item item, ItemLoot itemLoot) {
-			if (item.type == ItemID.Present) {
-				itemLoot.Add(ItemDropRule.OneFromOptions(3, new int[] {
-					ModContent.ItemType<SantasLittleHelper>()
-				}));
-			}
 		}
 	}
 }
